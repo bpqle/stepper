7774a25e8e9aedaa29970062c9ed96b3818dd05a
Bao Le <uac6qw@virginia.edu>
2021-09-21 13:50:19 -0400

    stable main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,64 @@
-use gpio_cdev::{Chip, LineRequestFlags, EventRequestFlags, EventType};
-use quicli::prelude::*;
-use std::thread::sleep;
-use std::time::Duration;
-use structopt::StructOpt;
+use gpio_cdev::{Chip, LineRequestFlags};
 
 fn main() {
-    let mut chip1 = Chip::new("/dev/gpiochip1");
-    let mut chip3 = Chip::new("/dev/gpiochip3");
+    let mut chip1 = Chip::new("/dev/gpiochip1")
+        .expect("Failed to get chip1");
+    let mut chip3 = Chip::new("/dev/gpiochip3")
+        .expect("Failed to get chip3");
+
+    let motor1_offsets : [u32;2] = [13,12];
+    let motor3_offsets : [u32;2] = [19,21];
+    let switch_offsets : [u32;2] = [14,15];
 
     let motor_handle_1 = chip1
-        .get_lines(vec![13, 12])
-        .request(LineRequestFlags::OUTPUT, 0, "stepper")?;
+        .get_lines(&motor1_offsets)
+        .expect("Couldn't get lines 13&12 for motor1")
+        .request(LineRequestFlags::OUTPUT, &[1,1], "stepper")
+        .expect("Couldn't request lines 13&12 for motor1");
     let motor_handle_3 = chip3
-        .get_lines(vec![19, 21])
-        .request(LineRequestFlags::OUTPUT, 0, "stepper")?;
+        .get_lines(&motor3_offsets)
+        .expect("Couldn't get lines 19&21 for motor3")
+        .request(LineRequestFlags::OUTPUT, &[1,1], "stepper")
+        .expect("Couldn't request lines 19&21 for motor3");
 
     let switch_handle_1 = chip1
-        .get_lines(vec![14, 15])
-        .request(LineRequestFlags::INPUT, 0, "switch")?;
+        .get_lines(&switch_offsets)
+        .expect("Couldn't get lines 14&15 for switch")
+        .request(LineRequestFlags::INPUT, &[1,1], "switch")
+        .expect("Couldn't request lines 14&15 for switch");
 
     let dt = 1000000 / 500;
     let num_half_steps = 8;
-    let mut step_values: &Vec<i8>;
+    let mut step_values1: &[u8;2];
+    let mut step_values3: &[u8;2];
     let mut step: u8 = 0;
 
-    let all_off :Vec<i8> = vec![0,0,0,0];
-    let _full_steps = vec![
-        vec![0,1,1,0],
-        vec![0,1,0,1],
-        vec![1,0,0,1],
-        vec![1,0,1,0],
-    ];
-    let half_steps: Vec<Vec<i8>> = vec![
-        vec![0,1,1,0],
-        vec![0,1,0,0],
-        vec![0,1,0,1],
-        vec![0,0,0,1],
-        vec![1,0,0,1],
-        vec![1,0,0,0],
-        vec![1,0,1,0],
-        vec![0,0,1,0],
-    ];
-
+    let all_off : [u8; 2]  = [0,0];
+    let half_steps_1 : [[u8;2];8] = [[0,1],[0,1],[0,1],[0,0],[1,0],[1,0],[1,0],[0,0]];
+    let half_steps_3 : [[u8;2];8] = [[1,0],[0,0],[0,1],[0,1],[0,1],[0,0],[1,0],[1,0]];
 
     println!("- initialized GPIOs \n");
     println!("- step dt = {:?} us\n", dt);
 
     loop {
-        let mut switch_values: Vec<u8> = switch_handle_1.get_values()
+        let switch_values: Vec<u8> = switch_handle_1.get_values()
             .expect("Failed to get switch values\n");
         if switch_values[0] == 0 {
             step = (step + 1) % &num_half_steps;
-            step_values = &half_steps[&step];
+            step_values1 = &half_steps_1[usize::from(step)];
+            step_values3 = &half_steps_3[usize::from(step)];
         } else if  switch_values[1] == 0 {
             step = (step - 1) % &num_half_steps;
-            step_values = &half_steps[&step];
+            step_values1 = &half_steps_1[usize::from(step)];
+            step_values3 = &half_steps_3[usize::from(step)];
         } else {
-            step_values = &all_off;
+            step_values1 = &all_off;
+            step_values3 = &all_off;
         }
-        motor_handle_1.set_values(&step_values);
-        motor_handle_3.set_values(&step_values);
+        motor_handle_1.set_values(step_values1)
+            .expect("Cannot set value for motor 1");
+        motor_handle_3.set_values(step_values3)
+            .expect("Cannot set value for motor 3");
     }
 
 }
