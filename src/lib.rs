8a835a78cebbdd5338ec48c709a9c94540f6ef0b
Bao Le <uac6qw@virginia.edu>
2021-09-21 16:45:30 -0400

    unstable refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,134 @@
-//struct Stepper2 {
-//    num_steps: u32,
-//    motor_pin_1: u8,
-//    motor_pin_2: u8,
-//}
+use gpio_cdev::{Chip, LineRequestFlags, EventRequestFlags, EventType, errors::Error as GpioError, MultiLineHandle};
+use thiserror;
 
-//struct Stepper4 {
-//    num_steps: u32,
-//    motor_pin_1: u8,
-//    motor_pin_2: u8,
-//    motor_pin_3: u8,
-//    motor_pin_4: u8,
-//}
+struct LinesValue([u8; 2]);
+pub struct StepperMotorApparatus {
+    chip1: Chip,
+    chip3: Chip,
+    stepper_motor: StepperMotor,
+    switch: Switch,
+}
+pub struct StepperMotor {
+    motor_1_handle: MultiLineHandle,
+    motor_3_handle: MultiLineHandle,
+}
 
+pub struct Switch {
+    handle: MultiLineHandle,
+}
+
+impl StepperMotor {
+     const MOTOR1_OFFSETS: [u32;2] = [13,12];
+     const MOTOR3_OFFSETS: [u32;2] = [19,21];
+     const ALL_OFF: LinesValue = LinesValue([0,0]);
+     const HALF_STEPS: [(LinesValue, LinesValue); 8] = [
+        (LinesValue([0,1]),LinesValue([1,0])),
+        (LinesValue([0,1]),LinesValue([0,0])),
+        (LinesValue([0,1]),LinesValue([0,1])),
+        (LinesValue([0,0]),LinesValue([0,1])),
+        (LinesValue([1,0]),LinesValue([0,1])),
+        (LinesValue([1,0]),LinesValue([0,0])),
+        (LinesValue([1,0]),LinesValue([1,0])),
+        (LinesValue([0,0]),LinesValue([1,0]))
+     ];
+
+    pub fn new(chip1: &mut Chip, chip3: &mut Chip) -> Result<Self, Error> {
+        let motor_handle_1 = chip1
+            .get_lines(&Self::MOTOR1_OFFSETS)
+            .map_err(|e:GpioError| Error::LinesGetError {source: e, lines: &Self::MOTOR1_OFFSETS})?
+            .request(LineRequestFlags::OUTPUT, &[0,0], "stepper")
+            .map_err(|e:GpioError| Error::LinesReqError {source: e, lines: &Self::MOTOR1_OFFSETS})?;
+
+        let motor_handle_3 = chip3
+            .get_lines(&Self::MOTOR3_OFFSETS)
+            .map_err(|e:GpioError| Error::LinesGetError {source: e, lines: &Self::MOTOR3_OFFSETS})?
+            .request(LineRequestFlags::OUTPUT, &[0,0], "stepper")
+            .map_err(|e:GpioError| Error::LinesReqError {source: e, lines: &Self::MOTOR3_OFFSETS})?;
+
+        Ok(StepperMotor {
+            motor_1_handle,
+            motor_3_handle,
+        })
+    }
+    pub fn set_state(state: State) -> Result<(), Error> {
+        let dt = 1000000 / 500;
+        let num_half_steps:usize = 8;
+        let step_values1: &LinesValue;
+        let step_values3: &LinesValue;
+        let mut step: usize = 0;
+
+        match state {
+            State::Forward => {
+
+            }
+            State::Backward => {}
+            State::Stop => {
+                motor_1_handle.set_values
+            }
+        }
+    }
+}
+impl Switch {
+    const SWITCH_OFFSETS: [u32;2] = [14,15];
+    pub fn new(chip1: &mut Chip) -> Result<Self, Error> {
+        let handle = chip1
+            .get_lines(&Self::SWITCH_OFFSETS)
+            .map_err(|e:GpioError| Error::LinesGetError {source: e, lines: &Self::SWITCH_OFFSETS})?
+            .request(LineRequestFlags::INPUT, &[0,0], "stepper")
+            .map_err(|e:GpioError| Error::LinesReqError {source: e, lines: &Self::SWITCH_OFFSETS})?;
+        Ok(Self{
+            handle
+        })
+    }
+    pub fn monitor()
+}
+impl StepperMotorApparatus {
+    pub fn new(chip1 : &str, chip3 : &str) -> Result<Self, Error> {
+        let mut chip1 = Chip::new(chip1).map_err( |e:GpioError|
+            Error::ChipError {source: e,
+                chip: ChipNumber::Chip1}
+        )?;
+        let mut chip3 = Chip::new(chip3).map_err( |e:GpioError|
+            Error::ChipError {source: e,
+                chip: ChipNumber::Chip3}
+        )?;
+        let stepper_motor = StepperMotor::new(&mut chip1, &mut chip3)?;
+        let switch = Switch::new(&mut chip1)?;
+        Ok(StepperMotorApparatus{
+            chip1,
+            chip3,
+            stepper_motor,
+            switch,
+        })
+    }
+}
+
+pub enum State{
+    Forward,
+    Backward,
+    Stop,
+}
+
+#[derive(Debug)]
+pub enum ChipNumber {
+    Chip1,
+    Chip3
+}
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Failed to get chip {chip:?}")]
+    ChipError {
+        source: GpioError,
+        chip: ChipNumber,
+    },
+    #[error("Failed to get lines")]
+    LinesGetError {
+        source: GpioError,
+        lines: &'static [u32; 2],
+    },
+    #[error("Failed to request lines")]
+    LinesReqError {
+        source: GpioError,
+        lines: &'static [u32; 2],
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,15 @@
 use gpio_cdev::{Chip, LineRequestFlags};
+use stepper::StepperMotorApparatus;
+
+
 
 fn main() {
-    let mut chip1 = Chip::new("/dev/gpiochip1")
-        .expect("Failed to get chip1");
-    let mut chip3 = Chip::new("/dev/gpiochip3")
-        .expect("Failed to get chip3");
+   let mut motor = StepperMotorApparatus::new("/dev/gpiochip1","/dev/gpiochip3")
+       .expect("Couldn't get either motors");
 
-    let motor1_offsets : [u32;2] = [13,12];
-    let motor3_offsets : [u32;2] = [19,21];
-    let switch_offsets : [u32;2] = [14,15];
 
-    let motor_handle_1 = chip1
-        .get_lines(&motor1_offsets)
-        .expect("Couldn't get lines 13&12 for motor1")
-        .request(LineRequestFlags::OUTPUT, &[1,1], "stepper")
-        .expect("Couldn't request lines 13&12 for motor1");
-    let motor_handle_3 = chip3
-        .get_lines(&motor3_offsets)
-        .expect("Couldn't get lines 19&21 for motor3")
-        .request(LineRequestFlags::OUTPUT, &[1,1], "stepper")
-        .expect("Couldn't request lines 19&21 for motor3");
 
-    let switch_handle_1 = chip1
-        .get_lines(&switch_offsets)
-        .expect("Couldn't get lines 14&15 for switch")
-        .request(LineRequestFlags::INPUT, &[1,1], "switch")
-        .expect("Couldn't request lines 14&15 for switch");
 
-    let dt = 1000000 / 500;
-    let num_half_steps = 8;
-    let mut step_values1: &[u8;2];
-    let mut step_values3: &[u8;2];
-    let mut step: u8 = 0;
-
-    let all_off : [u8; 2]  = [0,0];
-    let half_steps_1 : [[u8;2];8] = [[0,1],[0,1],[0,1],[0,0],[1,0],[1,0],[1,0],[0,0]];
-    let half_steps_3 : [[u8;2];8] = [[1,0],[0,0],[0,1],[0,1],[0,1],[0,0],[1,0],[1,0]];
 
     println!("- initialized GPIOs \n");
     println!("- step dt = {:?} us\n", dt);
@@ -45,19 +19,19 @@ fn main() {
             .expect("Failed to get switch values\n");
         if switch_values[0] == 0 {
             step = (step + 1) % &num_half_steps;
-            step_values1 = &half_steps_1[usize::from(step)];
-            step_values3 = &half_steps_3[usize::from(step)];
+            step_values1 = &half_steps[step].0;
+            step_values3 = &half_steps[step].1;
         } else if  switch_values[1] == 0 {
             step = (step - 1) % &num_half_steps;
-            step_values1 = &half_steps_1[usize::from(step)];
-            step_values3 = &half_steps_3[usize::from(step)];
+            step_values1 = &half_steps[step].0;
+            step_values3 = &half_steps[step].1;
         } else {
             step_values1 = &all_off;
             step_values3 = &all_off;
         }
-        motor_handle_1.set_values(step_values1)
+        motor_handle_1.set_values(&step_values1.0)
             .expect("Cannot set value for motor 1");
-        motor_handle_3.set_values(step_values3)
+        motor_handle_3.set_values(&step_values3.0)
             .expect("Cannot set value for motor 3");
     }
 
